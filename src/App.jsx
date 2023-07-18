57ef216ac5030e381fc36bc6d547ffe13775e3bc
Bacchern <139548074+Bacchern@users.noreply.github.com>
2023-07-18 14:23:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./assets/css/taildwind.css";
+import "./App.css";
+
+import Header from "./components/Header";
+
+
+
+
+const attri = {
+  background:'red',
+  color:'black'
+}
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+
+
+const ListHeader = [
+  {
+    name: "Buy Crypto",
+    id: 1,
+    isActve: false,
+  },
+  {
+    name: "Markets",
+    id: 2,
+  },
+  {
+    name: "Seed Sales",
+    id: 3,
+  },
+  {
+    name: "Swap",
+    id: 4,
+  },
+  {
+    name: "Wallet",
+    id: 5,
+    isActve: true,
+  },
+  {
+    name: "P2P",
+    id: 6,
+    isActve: true,
+  },
+  {
+    name: "Activity",
+    id: 7,
+    isActve: true,
+  },
+  {
+    name: "NFT",
+    id: 8,
+  },
+  {
+    name: "Content",
+    id: 9,
+    isActive: false,
+  },
+]
+
+export default function Header() {
+  return (
+   <div>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+    <div></div>
+   </div>
+  )
+}
